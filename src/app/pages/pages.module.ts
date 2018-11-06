5d2eb80a56056e8a360b2bd6f5a1392de879e335
Alan Islas <alan.islas.itic@gmail.com>
2018-11-06 14:23:03 -0600

    Creando barra de progreso

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
@@ -16,7 +18,8 @@ import { PAGES_ROUTING } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTING
+        PAGES_ROUTING,
+        FormsModule
     ],
     exports: [
         DashboardComponent,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  porcentaje = 50;
+
   constructor() { }
+
+  cambiarValor( valor ) {
+    this.porcentaje += valor;
+    if (this.porcentaje > 100) {
+      this.porcentaje = 100;
+      return;
+    } else if ( this.porcentaje < 0 ) {
+      this.porcentaje = 0;
+      return;
+    }
+  }
 
   ngOnInit() {
   }
